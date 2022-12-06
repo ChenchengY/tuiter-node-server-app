47c3d6c7c4f249a8993966cdc38d491c6fb2d822
ChenchengY <yichencheng@outlook.com>
2022-12-06 16:31:40 -0500

    try 3

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ import mongoose from "mongoose";
 
 
 const CONNECTION_STRING = process.env.DB_CONNECTION_STRING
- || 'mongodb://127.0.0.1:27017/tuiter'
+ || 'mongodb://localhost:27017/tuiter'
 mongoose.connect(CONNECTION_STRING);
 
 // mongoose.connect('mongodb+srv://bcarter1:<password>@cluster0.t22rzg9.mongodb.net/?retryWrites=true&w=majority');
